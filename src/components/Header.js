13c69861935d5aea1bffc01ebf0e118805eda384
Kristina Pang <kvypang@gmail.com>
2022-06-01 16:56:45 -0700

    main page layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ const Header = () => {
     <div className="header-wrapper">
     <div className="main-info">
       <h1>Kristina V. Pang</h1>
+      <a href="https://docs.google.com/document/d/16ixMc17ordpjh1S-fWbXzLxdjVfdbb_n1KxAWXU80PE/edit?usp=sharing" className="btn-resume bg-black text-white">My Resume</a>
       </div></div>
 ) 
 }
