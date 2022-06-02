d41c245d3050aa9913b852c8b5fa96852852ccc8
Kristina Pang <kvypang@gmail.com>
2022-06-01 22:16:32 -0700

    contact form and styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,20 @@ import React from 'react'
 
 function Contact() {
   return (
-      <h1>Contact Me</h1>
+      <div className="form">
+              <h1>Contact Me</h1>
+      <form>
+          <label>Your Name</label>
+          <input type="text"></input>
+          <label>Email Address</label>
+          <input type="email"></input>
+          <label>Subject</label>
+          <input type="text"></input>
+          <label>Message</label>
+          <textarea rows="6" placeholder="Enter your message here"/>
+          <button className="submitBtn">Submit</button>
+      </form>
+  </div>
 )
 }
 export default Contact
