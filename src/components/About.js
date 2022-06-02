c16a1c1b393ac0179a8f25514b57283c9b0a65ce
Kristina Pang <kvypang@gmail.com>
2022-06-01 22:31:21 -0700

    about me paragraph and styling

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 function About() {
   return (
+    <div className="container" id="aboutMe">
       <h1>About Me</h1>
+      <div className="container" id="about">
+<p>For the past 6 years, I have been in the real estate and finance industry from legal assisting, transaction coordinating, to loan closing. Through my professional career, I have most enjoyed analytics, problem-solving, and detail-oriented processes. 
+I am seeking employment long-term in software development, where I believe my skillset will be best suited and personally fulfilling while continuing to gain experience, and further develop my skills.</p>
+      </div></div>
 )
 }
 export default About
