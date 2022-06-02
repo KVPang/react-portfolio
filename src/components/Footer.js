c14be5d753abc085eb633ef0d9acecddb2806bcf
Kristina Pang <kvypang@gmail.com>
2022-06-02 08:28:51 -0700

    links to linkedin and github added to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,15 @@ const Footer = () => {
     return (
         <div className="footer">
             <footer className="bg-black text-center text-white" id="footerName">
-            Kristina V. Pang Portfolio | 2022 </footer>
+                Kristina V. Pang Portfolio | 2022
+                <section id="contact-me">
+                    <ul>
+                        <li><a href="mailto:kvypang@gmail.com" target="_blank">E-Mail</a></li>
+                        <li><a href="https://www.linkedin.com/in/kvpang/" target="_blank">LinkedIn</a></li>
+                        <li><a href="https://github.com/KVPang" target="_blank">GitHub</a></li>
+                    </ul>
+                </section>
+            </footer>
         </div>
     );
 }
