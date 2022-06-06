e757f75206a0f27732c9bc94a9214863dfabb161
Kristina Pang <kvypang@gmail.com>
2022-06-05 22:05:43 -0700

    project page layout and styling

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,7 +3,7 @@ import React from "react";
 const Resume = () => {
     return ( 
         <div className="resumePage" >
-            <h2>Resume</h2>
+            <h1>Resume</h1>
             <div className="container" id="resumeContent">
             <p> Front-end web developer with advancing skills in HTML, CSS and JavaScript. My strengths include problem solving, meeting deadlines, and attention to detail with strong aptitude for project management. I am looking forward to developing responsive websites and have a passion for collaborating with others to accomplish all goals. I thrive in fast-paced environments and I am excited to apply my knowledge and experience while continuing to further develop my skills. </p>
             <h4></h4>
